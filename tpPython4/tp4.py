441207d08b03db467d1b332d6ebafc54ff4c8641
ngamel <ngamel@f1.fdsetu.infra.umontpellier.fr>
2018-10-23 10:13:44 +0200

    suite tp4

---
+++ tpPython4/tp4.py
@@ -0,0 +1,66 @@
+﻿#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+import os
+import re
+
+liste= []
+
+connexions={}
+
+#fd = open("last.txt","r")
+#for ligne in fd.readlines():
+#	liste.append(ligne)
+#fd.close()
+
+liste = os.popen("last")
+
+for l in liste:
+        #print(l)
+        login = re.search("(^.*?)\s",l).group(1)
+        date=re.search("[A-Z][a-z]{2}.*?\d\d?",l).group(0)
+        duree = re.search("\((\d{2})[\+:](\d{2}).?(.?.?)",l)
+        dureeSep=[duree.group(1),duree.group(2), duree.group(3)]
+        #print(dureeSep)
+        #print(login)
+        if login:
+                print(login)
+                if login in connexions.keys():
+                        if date:
+                                print(date)
+                                if date in connexions[login].keys():
+                                        if dureeSep[2]!='':
+                                                #print(dureeSep[2])
+                                                connexions[login][date]+= int(dureeSep[2])*24*60 + int(dureeSep[1]) +int(dureeSep[0])*60
+                                        else:
+                                                connexions[login][date] += int(dureeSep[1]) +int(dureeSep[0])*60
+                                else:
+                                        if dureeSep[2]!='':
+                                                #print(dureeSep[2])
+                                                connexions[login][date]= int(dureeSep[2])*24*60 + int(dureeSep[1]) +int(dureeSep[0])*60
+                                        else:
+                                                connexions[login][date] = int(dureeSep[1]) +int(dureeSep[0])*60
+                else:
+                        connexions[login] = {}
+                        if date:
+                                print(date)
+                                if date in connexions[login].keys():
+                                        if dureeSep[2]!='':
+                                                #print(dureeSep[2])
+                                                connexions[login][date]+= int(dureeSep[2])*24*60 + int(dureeSep[1]) +int(dureeSep[0])*60
+                                        else:
+                                                connexions[login][date] += int(dureeSep[1]) +int(dureeSep[0])*60
+                                else:
+                                        if dureeSep[2]!='':
+                                                #print(dureeSep[2])
+                                                connexions[login][date]= int(dureeSep[2])*24*60 + int(dureeSep[1]) +int(dureeSep[0])*60
+                                        else:
+                                                connexions[login][date] = int(dureeSep[1]) +int(dureeSep[0])*60
+                        
+
+print(connexions["mjulien"]["Mon Oct 22"])
+                        
+                                        
+        
+                
+                
